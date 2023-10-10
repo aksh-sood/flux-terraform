a5cc3c29a33e8e10fe20ba77817f3064f36b5d66
aksh sood <aksh.sood@batonsystems.com>
2023-10-10 16:57:12 +0530

    one

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-# provider "aws" {
-#   region = "us-east-1"  # Change to your desired AWS region
-# }
-
-# resource "aws_instance" "example" {
-#   ami           = "ami-067d1e60475437da2"
-#   instance_type = "t2.micro"
-#   subnet_id = "subnet-03e317140cf1b1fdc"
-#   tags ={
-#     Name="Aksh"
-#   }
-# }
-
-variable "region" { default = "eu-central-1" }
- 
 provider "aws" {
- region     = var.region
+  region = "us-east-1"  # Change to your desired AWS region
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-067d1e60475437da2"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03e317140cf1b1fdc"
+  tags ={
+    Name="Aksh Flux"
+  }
+}
+
+# variable "region" { default = "eu-central-1" }
  
-resource "aws_s3_bucket" "wego-tfc-test-s3" {
-   bucket = "wego-tfc-test-s3"
-   acl = "private"  
-}
+# provider "aws" {
+#  region     = var.region
+# }
+ 
+# resource "aws_s3_bucket" "wego-tfc-test-s3" {
+#    bucket = "wego-tfc-test-s3"
+#    acl = "private"  
+# }
