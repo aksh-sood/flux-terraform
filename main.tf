c1a9d5fe682b07334a8761c1fa64d6b60f6508df
aksh sood <aksh.sood@batonsystems.com>
2023-10-10 16:41:23 +0530

    s3 for admin

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
-}
+# provider "aws" {
+#   region = "us-east-1"  # Change to your desired AWS region
+# }
 
-resource "aws_instance" "example" {
-  ami           = "ami-067d1e60475437da2"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-03e317140cf1b1fdc"
-  tags ={
-    Name="Aksh"
-  }
+# resource "aws_instance" "example" {
+#   ami           = "ami-067d1e60475437da2"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-03e317140cf1b1fdc"
+#   tags ={
+#     Name="Aksh"
+#   }
+# }
+
+variable "region" { default = "eu-central-1" }
+ 
+provider "aws" {
+ region     = var.region
+}
+ 
+resource "aws_s3_bucket" "wego-tfc-test-s3" {
+   bucket = "wego-tfc-test-s3"
+   acl = "private"  
 }
