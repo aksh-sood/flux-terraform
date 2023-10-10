efb7ab0d26b3cb687a5d4a2235b33eb6abfdee71
aksh sood <aksh.sood@batonsystems.com>
2023-10-10 16:36:58 +0530

    ec2 with subnet

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-variable "region" { default = "eu-central-1" }
- 
 provider "aws" {
- region     = var.region
+  region = "us-east-1"  # Change to your desired AWS region
 }
- 
-resource "aws_s3_bucket" "flux-s3" {
-   bucket = "aksh-flux"
-   acl = "private"  
+
+resource "aws_instance" "example" {
+  ami           = "ami-067d1e60475437da2"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03e317140cf1b1fdc"
+  tags ={
+    Name="Aksh"
+  }
 }
